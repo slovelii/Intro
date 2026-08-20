0b18e79421e40eff7fd0d5b1edcca0e04ddacbb9
slovelii <susanamarinurrego016@gmail.com>
2026-08-20 13:27:38 -0500

    Perrito

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ st.header ("Aquí voy a poner un perrito con sombrero")
 st.write("Porque me gustan mucho los perritos")
 image = Image.open("PerritoConSombrero.jpeg")
 st.image(image, caption = "Perrito con sombrero")
+
+texto = st.text_input ("Escribe algo","Este es mi texto")
+st.write("El texto escrito es", texto)
+
+st.subheader("Ahora 2 columnas")
+col1, col2 = st.columns(2)

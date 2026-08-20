bc431c742370dbd5f78ea1d6ac5b3cd50269c054
slovelii <susanamarinurrego016@gmail.com>
2026-08-20 13:47:53 -0500

    Add multiple images and update favorite dog logic

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import streamlit as st
 from PIL import Image
+
+image = Image.open("PerritoConSombrero.jpeg")
+image2 = Image.open("Perrito que mira de lado.jpg")
+image3 = Image.open("Perrito feliz.jpeg")
+image4 = Image.open("Perritos amigos.jpg")
 st.title ("Perrito con sombrero")
 
 st.header ("Aquí voy a poner un perrito con sombrero")
 st.write("Porque me gustan mucho los perritos")
-image = Image.open("PerritoConSombrero.jpeg")
+
 st.image(image, caption = "Perrito con sombrero")
 
 texto = st.text_input ("Escribe algo","Este es mi texto")
@@ -25,7 +30,10 @@ with col2:
   modo = st.radio("¿Cuál es tu perrito favorito?", ("Perrito mirando feo", "Perrito feliz", "Perritos amigos"))
   if modo == "Perrito mirando feo" :
     st.write("Tu favorito es el perrito mirando feo")
+    st.image(image2, caption = "Perrito con sombrero")
   if modo == "Perrito feliz" :
     st.write("Tu favorito es el perrito feliz")
+    st.image(image3, caption = "Perrito con sombrero")
   if modo == "Perritos amigos" :
-    st.write("Tu favorito son los perritos amigos)
+    st.write("Tu favorito son los perritos amigos")
+    st.image(image4, caption = "Perrito con sombrero")
